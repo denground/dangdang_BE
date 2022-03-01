4b1c276e8ee533a9fbc1b687f0b1cae71300c813
spirit-B <janghoon333@naver.com>
2022-03-01 16:29:58 +0900

    profile 수정

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const Profile = require("../schemas/profile");
-const upload = require("../modules/multer");
-const auth = require("../middlewares/auth-middleware");
+// const express = require("express");
+// const router = express.Router();
+// const Profile = require("../schemas/profile");
+// const upload = require("../modules/multer");
+// const auth = require("../middlewares/auth-middleware");
 
 // 견종 등록
 // router.post("/profiles", auth, async (req, res) => {
