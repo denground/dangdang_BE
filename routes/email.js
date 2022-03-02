d3d945432c3cbe209f2ab2f6661db3347d8896ec
spirit-B <janghoon333@naver.com>
2022-03-02 14:31:38 +0900

    오타 수정

--- routes/email.js
+++ routes/email.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const nodemailer = require('nodemailer')
-const User = require('../schemas/User')
+const User = require('../schemas/user')
 const CryptoJS = require("crypto-js");
 
 
