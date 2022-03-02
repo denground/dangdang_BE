f70f61445b57e823dbd9f5d1c53cd8e17dd82f99
spirit-B <janghoon333@naver.com>
2022-03-02 17:21:05 +0900

    app.js 재수정

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const connect = require('./schemas/index');
 const cors = require('cors');
 const app = express();
-const helmet = require('helmet')
 const port = process.env.PORT;
+const helmet = require('helmet');
 const userRouter = require('./routes/user');
 const guideRouter = require('./routes/guide');
 const profileRouter = require('./routes/profile');
 const mapRouter = require('./routes/map');
-const emailRouter = require('./routes/email');
-
 require('dotenv').config();
+
+connect();
 
 app.use((req, res, next) => {
   console.log(
@@ -23,18 +23,15 @@ app.use((req, res, next) => {
   next();
 });
 
-connect();
-
-app.use(cors({ 
-  origin: "http://localhost:3000",
-  credentials: true
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+  origin: "http://localhost:3000/",
+  credentials: true,
 }));
 app.use(helmet());
 
-app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter, emailRouter]);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 켜졌습니다.');
