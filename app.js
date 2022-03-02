228e04804c7e6d994e7abf5e7ad7bdb15cc9b5d0
spirit-B <janghoon333@naver.com>
2022-03-02 16:19:45 +0900

    cors 임시 설정

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ 
+  origin: "http://localhost:4200",
+  credentials: true
+}));
 
 app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter, emailRouter]);
 
