7cc180e44360ed0c39b738d66ed493bdc8d5a181
spirit-B <janghoon333@naver.com>
2022-03-02 14:28:55 +0900

    app.js 라우터 추가

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const userRouter = require('./routes/user');
 const guideRouter = require('./routes/guide');
 const profileRouter = require('./routes/profile');
 const mapRouter = require('./routes/map');
+const emailRouter = requier('./routes/email');
 
 require('dotenv').config();
 
 connect();
 
+app.use(helmet());
 app.use((req, res, next) => {
   console.log(
     'Request URL:',
@@ -27,9 +29,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(helmet());
 
-app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
+app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter, emailRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 켜졌습니다.');
