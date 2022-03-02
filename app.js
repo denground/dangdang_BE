64f42adde64b9d4c0a7a6a8a9468b2be8167e103
spirit-B <janghoon333@naver.com>
2022-03-03 00:46:50 +0900

    app.js prettier

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-const express = require('express');
-const connect = require('./schemas/index');
-const cors = require('cors');
+const express = require("express");
+const connect = require("./schemas/index");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
-const helmet = require('helmet');
-const userRouter = require('./routes/user');
-const guideRouter = require('./routes/guide');
-const profileRouter = require('./routes/profile');
-const mapRouter = require('./routes/map');
-require('dotenv').config();
+const helmet = require("helmet");
+const userRouter = require("./routes/user");
+const guideRouter = require("./routes/guide");
+const profileRouter = require("./routes/profile");
+const mapRouter = require("./routes/map");
+require("dotenv").config();
 
 connect();
 
-
 app.use((req, res, next) => {
-  console.log(
-    'Request URL:',
-    `[${req.method}]`,
-    req.originalUrl,
-    ' - ',
-    new Date().toLocaleString()
+    console.log(
+        "Request URL:",
+        `[${req.method}]`,
+        req.originalUrl,
+        " - ",
+        new Date().toLocaleString()
     );
     next();
-  });
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }));
-  app.disable("x-powered-by");
-  app.use(helmet());
-  
-  app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
-  
-  app.listen(port, () => {
-  console.log(port, '포트로 서버가 켜졌습니다.');
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+app.disable("x-powered-by");
+app.use(helmet());
+
+app.use("/api", [userRouter, guideRouter, profileRouter, mapRouter]);
+
+app.listen(port, () => {
+    console.log(port, "포트로 서버가 켜졌습니다.");
 });
