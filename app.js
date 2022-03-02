f0e298ec9d7e78f20bf1d6dc38853caaea507990
spirit-B <janghoon333@naver.com>
2022-03-02 17:26:34 +0900

    app.js 수정완료

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ require('dotenv').config();
 
 connect();
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
-app.use(helmet());
 
 app.use((req, res, next) => {
   console.log(
@@ -25,15 +20,20 @@ app.use((req, res, next) => {
     req.originalUrl,
     ' - ',
     new Date().toLocaleString()
-  );
-  next();
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
-
-app.listen(port, () => {
+    );
+    next();
+  });
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }));
+  app.use(helmet());
+  
+  app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
+  
+  app.listen(port, () => {
   console.log(port, '포트로 서버가 켜졌습니다.');
 });
