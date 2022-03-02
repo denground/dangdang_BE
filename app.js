8279f109de39b790fc1ae89d1cc13c99cf7d52e3
spirit-B <janghoon333@naver.com>
2022-03-02 20:24:14 +0900

    app.js 보안 추가

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     origin: "http://localhost:3000",
     credentials: true,
   }));
+  app.disable("x-powered-by");
   app.use(helmet());
   
   app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter]);
