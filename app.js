e6777dbb737ee25c3b8bd2498f02073ad56e667f
spirit-B <janghoon333@naver.com>
2022-03-02 16:25:53 +0900

    cors 재설정

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ 
-  origin: "http://localhost:4200",
+  origin: "http://localhost:3000",
   credentials: true
 }));
 
