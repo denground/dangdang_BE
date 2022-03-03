b963da7543a65d6149e8f8a0e0c2f3a25e82cd7e
nagitak <nagitak>
2022-03-03 22:19:01 +0900

    guideController testCode

--- controller/guide.js
+++ controller/guide.js
@@ -1,32 +1,34 @@
 const Guide = require('../schemas/guide');
 
-exports.guideMain = async (req, res) => {
-  // 게시글 데이터 추가가 필요할 경우 사용
-  // await Guide.create({
-  //     guideTitle: '강아지와 훈련??',
-  //     guideImage: 'https://t1.daumcdn.net/cfile/tistory/994C4B465E81F9A20A',
-  //     guideContent: '댕댕이와 훈련 ???'
-  // })
+exports.guideMain = async (req, res, next) => {
+  try {
+    // 게시글 데이터 추가가 필요할 경우 사용
+    // await Guide.create({
+    //     guideTitle: '강아지와 훈련??',
+    //     guideImage: 'https://t1.daumcdn.net/cfile/tistory/994C4B465E81F9A20A',
+    //     guideContent: '댕댕이와 훈련 ???'
+    // })
 
-  // Guide 스키마에 있는 정보 추출
-  const guide = await Guide.find({}, { guideContent: false });
-  // for (let i = 0; i < guide.length; i++) {
-  //     let title = guide[i].guideTitle
-  //     let image = guide[i].guideImage
-  //     console.log('title : ', title, 'image : ', image)
-  // }
-  res.status(200).json({
-    guide,
-  });
+    // Guide 스키마에 있는 정보 추출
+    const guide = await Guide.find({}, { guideContent: false });
+    // for (let i = 0; i < guide.length; i++) {
+    //     let title = guide[i].guideTitle
+    //     let image = guide[i].guideImage
+    //     console.log('title : ', title, 'image : ', image)
+    // }
+    res.status(200).json({
+      guide,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.guideDetail = async (req, res) => {
-  // url 값 가져오기
-  const _id = req.params.postNumber;
-  console.log(_id);
-  // 게시글 가져오기
-  const article = await Guide.findOne({ _id });
-  res.status(200).json({
-    article,
-  });
+  try {
+    const guide = await Guide.findById(req.params.postNumber);
+    res.status(200).json(guide);
+  } catch (error) {
+    next(error);
+  }
 };
--- test/unit/guide.test.js
+++ test/unit/guide.test.js
@@ -1,14 +1,12 @@
 const guideController = require('../../controller/guide');
 const guideModel = require('../../schemas/guide');
 const httpMocks = require('node-mocks-http');
+const allGuide = require('../data/all-guide.json');
 
-const productId = 'testCode';
-const updatedProduct = {
-  name: 'updated name',
-  description: 'updated description',
-};
+const postNumber = 'testCode';
 
 guideModel.find = jest.fn();
+guideModel.findById = jest.fn();
 
 beforeEach(() => {
   req = httpMocks.createRequest();
@@ -23,5 +21,45 @@ describe('Guide Controller GET', () => {
   test('should call guideModel.find({})', async () => {
     await guideController.guideMain(req, res, next);
     expect(guideModel.find).toHaveBeenCalledWith({});
+  });
+  test('should return 200 response', async () => {
+    await guideController.guideMain(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled).toBeTruthy();
+  });
+  test('should return json body in response', async () => {
+    await guideController.guideMain(req, res, next);
+    expect(res._getJSONData).toBeTruthy();
+  });
+  test('should handle error', async () => {
+    const errorMessage = { message: 'Error' };
+    const rejectedPromise = Promise.reject(errorMessage);
+    guideModel.find.mockReturnValue(rejectedPromise);
+    await guideController.guideMain(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+});
+
+describe('Guide Controller GetById', () => {
+  test('should have a getGuideById', () => {
+    expect(typeof guideController.guideDetail).toBe('function');
+  });
+  test('should call productModel.findeById', async () => {
+    req.params.postNumber = postNumber;
+    await guideController.guideDetail(req, res, next);
+    expect(guideModel.findById).toBeCalledWith(postNumber);
+  });
+  test('should return json body and response code 200', async () => {
+    await guideController.guideDetail(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData).toBeTruthy();
+    expect(res._isEndCalled).toBeTruthy();
+  });
+  test('should handle Error', async () => {
+    const errorMessage = { message: 'Error' };
+    const rejectedPromise = Promise.reject(errorMessage);
+    guideModel.findById.mockReturnValue(rejectedPromise);
+    await guideController.guideDetail(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
   });
 });
