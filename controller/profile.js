e14970387cbcb9fdb16ddea80f9dad60706becd2
nagitak <nagitak>
2022-03-04 15:43:42 +0900

    handle error

--- controller/profile.js
+++ controller/profile.js
@@ -1,27 +1,37 @@
 const Profile = require('../schemas/profile');
 
-exports.savedog = async (req, res) => {
+exports.savedog = async (req, res, next) => {
   const { petName, petGender, petBirth, petBreed } = req.body;
   const { user } = res.locals;
-  await Profile.create({
-    petImage: req.file.location,
-    petName,
-    petGender,
-    petBirth,
-    petBreed,
-    userID: user.userID,
-  });
-  res.status(200).send({ success: '정보가 등록되었습니다.' });
+  try {
+    await Profile.create({
+      petImage: req.file.location,
+      petName,
+      petGender,
+      petBirth,
+      petBreed,
+      userID: user.userID,
+    });
+    res.status(200).send({ success: '정보가 등록되었습니다.' });
+  } catch (error) {
+    res.status(400).send({ fail: '정보를 다시 확인해주세요' });
+    next(error);
+  }
 };
 
-exports.mypageMain = async (req, res) => {
+exports.mypageMain = async (req, res, next) => {
   const { user } = res.locals;
-  const [userData] = await Profile.find({ userID: user.userID });
-  console.log(user.nickname);
-  console.log(userData);
-  if (!userData) {
-    res.status(401).send('데이터가 존재하지 않습니다.');
-    return;
+  try {
+    const [userData] = await Profile.find({ userID: user.userID });
+    console.log(user.nickname);
+    console.log(userData);
+    if (!userData) {
+      res.status(401).send('데이터가 존재하지 않습니다.');
+      return;
+    }
+    res.status(200).send(userData);
+  } catch (error) {
+    res.status(400).send();
+    next(error);
   }
-  res.status(200).send(userData);
 };
