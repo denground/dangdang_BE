f9fc670734d6990b518b398df80d08d33b6a9243
spirit-B <janghoon333@naver.com>
2022-03-04 14:10:32 +0900

    test를 위해 cors origin 제거

--- app.js
+++ app.js
@@ -26,12 +26,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-    cors({
-        origin: "http://dengdeng.s3-website.ap-northeast-2.amazonaws.com/",
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(helmet());
 
 app.use("/api", [userRouter, guideRouter, profileRouter, mapRouter, emailRouter]);
