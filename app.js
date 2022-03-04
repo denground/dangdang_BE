fff483e7d400076257ab9f1bd99ee39d7c65df89
spirit-B <janghoon333@naver.com>
2022-03-04 13:45:33 +0900

    cors 프론트 배포 주소로 설정

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://dengdeng.s3-website.ap-northeast-2.amazonaws.com/",
         credentials: true,
     })
 );
