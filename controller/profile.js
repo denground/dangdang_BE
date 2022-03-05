54d9fdff6e37e1d32ea77750b91652dbac0176df
spirit-B <janghoon333@naver.com>
2022-03-05 17:14:50 +0900

    map 데이터 불러오기 코드 추가

--- controller/profile.js
+++ controller/profile.js
@@ -1,37 +1,39 @@
-const Profile = require('../schemas/profile');
+const Profile = require("../schemas/profile");
+const MapModel = require("../schemas/map");
 
 exports.savedog = async (req, res, next) => {
-  const { petName, petGender, petBirth, petBreed } = req.body;
-  const { user } = res.locals;
-  try {
-    await Profile.create({
-      petImage: req.file.location,
-      petName,
-      petGender,
-      petBirth,
-      petBreed,
-      userID: user.userID,
-    });
-    res.status(200).send({ success: '정보가 등록되었습니다.' });
-  } catch (error) {
-    res.status(400).send({ fail: '정보를 다시 확인해주세요' });
-    next(error);
-  }
+    const { petName, petGender, petBirth, petBreed } = req.body;
+    const { user } = res.locals;
+    try {
+        await Profile.create({
+            petImage: req.file.location,
+            petName,
+            petGender,
+            petBirth,
+            petBreed,
+            userID: user.userID,
+        });
+        res.status(200).send({ success: "정보가 등록되었습니다." });
+    } catch (error) {
+        res.status(400).send({ fail: "정보를 다시 확인해주세요" });
+        next(error);
+    }
 };
 
 exports.mypageMain = async (req, res, next) => {
-  const { user } = res.locals;
-  try {
-    const [userData] = await Profile.find({ userID: user.userID });
-    console.log(user.nickname);
-    console.log(userData);
-    if (!userData) {
-      res.status(401).send('데이터가 존재하지 않습니다.');
-      return;
+    const { user } = res.locals;
+    try {
+        const [userData] = await Profile.find({ userID: user.userID });
+        const [mapData] = await MapModel.find({ userID: user.userID });
+        console.log(user.nickname);
+        console.log(userData);
+        if (!userData) {
+            res.status(401).send({ fail: "데이터가 존재하지 않습니다." });
+            return;
+        }
+        res.status(200).send(userData, mapData);
+    } catch (error) {
+        res.status(400).send({ fail: "알 수 없는 오류가 발생했습니다." });
+        next(error);
     }
-    res.status(200).send(userData);
-  } catch (error) {
-    res.status(400).send();
-    next(error);
-  }
 };
