dc66293319072d1815c0b9629beeefba8e4ace20
nagitak <nagitak>
2022-03-09 14:29:05 +0900

    router 수정

--- routes/map.js
+++ routes/map.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleWare = require('../middlewares/auth-middleware');
-const upload = require('../modules/multer');
 const mapController = require('../controller/map');
-
-// 산책일지 저장 (description 추가 시)
-router.post('/maps/diary', authMiddleWare, mapController.updateMap);
 
 //반려동물 사진 보내주기 (일시정지 클릭 시)
 router.get('/maps/pause', authMiddleWare, mapController.showImage);
