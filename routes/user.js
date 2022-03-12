4b76b11838add6ea0b74d82c1d143bc88c4885d0
spirit-B <janghoon333@naver.com>
2022-03-13 00:06:14 +0900

    소셜로그인 console.log

--- routes/user.js
+++ routes/user.js
@@ -29,7 +29,7 @@ router.get(
         //     msg: '카카오 로그인 성공..!',
         //     token_cookie,
         // })
-        console.log(req.session);
+        console.log("token : " + req.session.token);
 
         res.cookie(process.env.COOKIE, token);
         res.redirect('https://quick-monkey-15.loca.lt/main');
