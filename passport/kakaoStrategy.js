e9ecfd6f243d38d125687ebdf9c985b525a1051d
spirit-B <janghoon333@naver.com>
2022-03-21 09:51:34 +0900

    카카오 로그인 최종

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -9,17 +9,10 @@ module.exports = (passport) => {
         callbackURL: process.env.CALLBACKURL,
     }, async (accessToken, refreshToken, profile, done) => {
         try {
-            const exUser = await User.findOne({email: profile._json.kakao_account.email});
-            console.log("카카오 시도 이메일: " + profile._json.kakao_account.email);
-            console.log("exUser : " + exUser);
+            const exUser = await User.findOne({userID: profile.id});
 
             if (exUser) {
-                console.log("provider : " + exUser.provider);
-                if (exUser.provider === "kakao") {
-                    done(null, exUser);
-                } else {
-                    done(null, false, { fail: "이미 카카오가 아닌 경로로 가입하셨습니다! 아이디, 비밀번호로 로그인 해주세요!"})
-                }
+                done(null, exUser);
             } else {
                 const newUser = await User.create({
                     userID: profile.id,
@@ -27,7 +20,6 @@ module.exports = (passport) => {
                     nickname: profile.displayName,
                     provider: "kakao",
                 });
-                console.log(newUser);
                 done(null, newUser);
             }
         } catch (error) {
