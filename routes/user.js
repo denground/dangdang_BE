066f37611f5338001dae00e89e4bf798b66378cd
nagitak <nagitak>
2022-03-21 10:29:56 +0900

    닉네임/ 패스워드 변경

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleWare = require('../middlewares/auth-middleware');
 const userController = require('../controller/user');
 const passport = require('passport');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // 회원가입 router
 router.post('/users/signup', userController.userSignup);
@@ -11,28 +11,40 @@ router.post('/users/signup', userController.userSignup);
 // 로그인 router
 router.post('/users/login', userController.login);
 
+// 닉네임 변경 router
+router.patch('/users/modifyNic', authMiddleWare, userController.modifyNicname);
+
+// 비밀번호 변경 router
+router.patch(
+    '/users/modiPassword',
+    authMiddleWare,
+    userController.modifyPassword
+);
+
 // 회원인증 router
 router.get('/users/auth', authMiddleWare, userController.auth);
 
 // passport-kakao Login
 router.get('/kakao', passport.authenticate('kakao'));
 router.get('/auth/kakao/callback', (req, res, next) => {
-    console.log("kakao callback 진입");
-    console.log("req.query : " + JSON.stringify(req.query));
-    passport.authenticate('kakao', {
-        failureRedirect: '/',
-    }, (err, user, info) => {
-        if (err) return res.status(401).send(err);
-        const { userID, nickname } = user;
-        const token = jwt.sign(
-            { userID: userID, nickname: nickname },
-            process.env.TOKEN_SECRET_KEY
-        );
-        console.log("kakao token: " + token);
-        res.send({ token, success: "카카오 로그인 성공!" });
-    }) (req, res, next);
+    console.log('kakao callback 진입');
+    console.log('req.query : ' + JSON.stringify(req.query));
+    passport.authenticate(
+        'kakao',
+        {
+            failureRedirect: '/',
+        },
+        (err, user, info) => {
+            if (err) return res.status(401).send(err);
+            const { userID, nickname } = user;
+            const token = jwt.sign(
+                { userID: userID, nickname: nickname },
+                process.env.TOKEN_SECRET_KEY
+            );
+            console.log('kakao token: ' + token);
+            res.send({ token, success: '카카오 로그인 성공!' });
+        }
+    )(req, res, next);
 });
 
 module.exports = router;
-
-
