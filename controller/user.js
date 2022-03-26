3a1d53fb48ee5857d75435b19f1c928f9b87418a
spirit-B <janghoon333@naver.com>
2022-03-26 18:55:50 +0900

    console 삭제

--- controller/user.js
+++ controller/user.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 
 exports.userSignup = async (req, res) => {
     try {
-        console.log("test");
         // Joi
         const userSchema = Joi.object({
             userID: Joi.string()
